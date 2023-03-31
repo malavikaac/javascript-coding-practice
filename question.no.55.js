dc487d2132b73af01ea9922a51f941c7648f1986
malavikaac <acmalavika@gmail.com>
2023-03-31 20:23:09 +0530

    Functions

--- question.no.55.js
+++ question.no.55.js
@@ -1,11 +1,11 @@
 // Setup
 const outerWear = "T-Shirt";
 
-function myOutfit() {
+function ourOutfit() {
   // Only change code below this line
 const outerWear ="sweater";
   // Only change code above this line
   return outerWear;
 }
 
-myOutfit();
+ourOutfit();
