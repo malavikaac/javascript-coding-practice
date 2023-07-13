3209e09430e88f4d2a4c82bcceb6a9718c2eedf7
malavikaac <acmalavika@gmail.com>
2023-07-13 17:46:07 +0100

    question - 20

--- question.no.65.js
+++ question.no.65.js
@@ -1,9 +1,9 @@
 // Setup
 function testStrictNotEqual(val) {
-  if (val!==17) { // Change this line
+  if (val!==20) { // Change this line
     return "Not Equal";
   }
   return "Equal";
 }
 
-testStrictNotEqual(17);
+testStrictNotEqual(20);
